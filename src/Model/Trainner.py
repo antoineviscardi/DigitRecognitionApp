957c23d9eb9880cf0cf1f23145abc6588f6fb4b0
Antoine Viscardi <antoine.viscardi@gmail.com>
2017-11-15 09:23:13 -0500

    Untangled code and added scalling of data.
    Created function to load data.
    Lack of scalling caused really poored results.

--- src/Model/Trainner.py
+++ src/Model/Trainner.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
 from sklearn.datasets import fetch_mldata
-from test import Network
+from Network import Network
 
 def print_digit(digit):
     """
@@ -20,26 +20,24 @@ def vectorize_label(j):
     e[int(j)] = 1.0
     return e
 
-
-def main():
-    """Fetching the MNIST dataset"""
-    mnist = fetch_mldata('MNIST original')
-
-##    """Print one digit for testing"""
-##    print_digit(mnist['data'][42000])
-
+def fetch_wrap_data():
     """The MNIST dataset contains 60'000 data points. The last 10'000 elements
     of the dataset are a prebuilt testing set."""
-    train_set = list(zip(mnist.data[:60000], mnist.target[:60000]))
-    test_set = list(zip(mnist.data[-10000:], mnist.target[-10000:]))
-    train_set = [(train_set[i][0].reshape(-1,1), vectorize_label(train_set[i][1]))
-                 for i in range(len(train_set))]
-    test_set = [(test_set[i][0].reshape(-1,1), vectorize_label(test_set[i][1]))
-                 for i in range(len(test_set))]
+    mnist = fetch_mldata('MNIST original')
+    mnist = list(zip(mnist.data, mnist.target))
+    mnist = [(mnist[i][0].reshape(-1,1)/255, vectorize_label(mnist[i][1]))
+             for i in range(len(mnist))]
+    train_set = mnist[:60000]
+    test_set = mnist[-10000:]
+    return (train_set, test_set)
 
-    network = Network([784, 30, 10])
-    network.SGD(train_set, 5, 10, 3.0)
-    print(network.evaluate(test_set))
+
+def main():
+    train_set, test_set = fetch_wrap_data()
+    network = Network([784, 100, 10])
+    network.train(train_set, 5, 10, 3.0)
+    print(network.test(test_set))
+    network.save('net.npz')
 
 if __name__ == "__main__":
     main()
